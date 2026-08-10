a4b09ff61563d69a8e4e9b2a7fc99126eab4bf1c
momin <mominkarim0305@gmail.com>
2026-08-10 12:32:08 +0500

    change undiq

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -1,42 +1,41 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document, Types } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
 
 export type UserDocument = User & Document;
 
 @Schema({ timestamps: true })
 export class User {
-
   @Prop({ type: Types.ObjectId, ref: User.name, required: false })
   UserId: Types.ObjectId; // Reference to User (user)
-  
+
   @Prop({ required: true })
   name: string; // Name
 
-  @Prop({ required: true, unique: true, lowercase: true })
+  @Prop({ lowercase: true })
   email: string; // Email
 
-  @Prop({ required: true })
+  @Prop({})
   phone: string; // Phone
 
-  @Prop({ required: true, unique: true })
+  @Prop({})
   cnic: string; // CNIC
 
   @Prop()
   address: string; // Address
 
-  @Prop({ default: "Customer" })
-  role: string; // 
+  @Prop({ default: 'Customer' })
+  role: string; //
 
-  @Prop({ default: "https://via.placeholder.com/100" })
+  @Prop({ default: 'https://via.placeholder.com/100' })
   image: string; // Profile image
 
   @Prop({ type: Date, default: Date.now })
   lastLogin: Date; // last login tracking
 
-  @Prop({ required: true, default: "Active" })
+  @Prop({ required: true, default: 'Active' })
   status: string; // Active / Inactive
 
-  @Prop({ default: "123456" })
+  @Prop({ default: '123456' })
   password: string; // Hashed password
 
   // OTP fields for authentication flows
